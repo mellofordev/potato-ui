bfc6557176c05fd183e3adbf2ff543b801b90bcc
Sreedhar <sreedharks2003@gmail.com>
2021-10-25 09:54:28 +0530

    Optional field Bug fix

--- Components/screens/Post.js
+++ Components/screens/Post.js
@@ -72,7 +72,7 @@ export default function Post(){
             type:'image/'+imgType,
           });
           }else{
-              formdata.append('pic',null);
+              formdata.append('pic','');
           }
   
 

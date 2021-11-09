77087afb6cfc9f9608dd8cf3dd3b2c8b2cdd88a3
Sreedhar <sreedharks2003@gmail.com>
2021-11-10 00:17:30 +0530

    flatlist to scrollview migration

--- Components/screens/StackComment.js
+++ Components/screens/StackComment.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect,useRef,useCallback} from 'react';
-import {View,StyleSheet,Text,Image,FlatList,Dimensions,Alert, TouchableOpacity} from 'react-native';
+import {View,StyleSheet,Text,Image,RefreshControl,Dimensions,Alert, TouchableOpacity, ScrollView} from 'react-native';
 import { ActivityIndicator,TextInput,Divider} from 'react-native-paper';
 import { Modalize } from 'react-native-modalize';
 import RButton from '../RButtonComponent';
@@ -12,6 +12,7 @@ export default function StackComment({route}){
     const [data,setData]=useState([]);
     const [text,setText]=useState('');
     const [btn,setBtn]=useState(false);
+    const [isRefreshing,setIsRefreshing]=useState(false);
     const modalizeRef = useRef(null);
     const onOpen=()=>{
         modalizeRef.current?.open()
@@ -39,7 +40,7 @@ export default function StackComment({route}){
         .catch(error=>{setError(error)})
     }
     const postreq=()=>{
-        console.log('pressed');
+        
         setBtn(true);
         fetch('https://punfuel.pythonanywhere.com/api/comment/'+route.params.id+'/',{
             method:'POST',
@@ -77,45 +78,66 @@ export default function StackComment({route}){
     const wait = (timeout) => {
         return new Promise(resolve => setTimeout(resolve, timeout));
     }
-
+    const EmptyComment=()=>{
+        return(
+        <View style={{justifyContent:'center',alignItems:'center',marginTop:30}}>
+                    
+            <Text style={{textAlign:'center',fontSize:18}}>Be the first to comment for this post</Text>
+        </View>
+        );
+    }
+    const onRefresh = useCallback(()=>{
+        setIsRefreshing(true);
+        wait(2000).then(()=>{
+            setIsRefreshing(false);
+            
+        })
+        apireq();
+    },[])
+    const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
+        const paddingToBottom = 20;
+        return layoutMeasurement.height + contentOffset.y >=
+          contentSize.height - paddingToBottom;
+      };
     return(
         <>
         <View style={styles.container}>
-         {!isloading? <ActivityIndicator size={24} color='#7289DA' style={{marginTop:35}}/> :(  
-        <FlatList
-         data={data}
-         renderItem={({item})=>{
-        return(
-        <View style={styles.bottomsheetcontainer}>
-            <View style={styles.box}>
-                <Image style={{marginRight:15,height:45,width:45,borderRadius:10}} source={{uri:'https://punfuel.pythonanywhere.com/media/'+item.profile_pic}}/>
-                
-                <Text style={{marginRight:3,color:'#23272A',fontWeight:'900',fontFamily:'Roboto'}}>{(item.user).length>10 ?(item.user).slice(0,10)+'...':((item.user))}</Text>
-                {item.verified==true &&
-                <Image style={{width:16,height:16,marginTop:3,marginLeft:3}} source={{uri:'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/twitter_verified.png'}}/>
-                }
-            </View>
-            <View style={{flexDirection:'column',marginLeft:55,top:-17}}>
-                    <Text style={{fontSize:16}}>{item.comment}</Text>
-            </View>
-        </View>);
+         {!isloading && <ActivityIndicator size={24} color='#7289DA' style={{marginTop:35}}/> }
+         {data==[] && <EmptyComment/>}
+         <ScrollView onScroll={({nativeEvent})=>{
+             if(isCloseToBottom){
+                 console.log('fetchmore');
+             }
          }}
-        keyExtractor={(item,index)=>index.toString()}
-        ListEmptyComponent={()=>{
-            return(
-                <View style={{justifyContent:'center',alignItems:'center',marginTop:30}}>
+         refreshControl={
+            <RefreshControl 
+            enabled={true} 
+            refreshing={isRefreshing}
+            onRefresh={onRefresh}/>}
+
+         > 
+         {data!=[] &&data.map((i,item)=>{
+             
+             return(
+                <View style={styles.bottomsheetcontainer} key={item}>
+                <View style={styles.box}>
+                    <Image style={{marginRight:15,height:45,width:45,borderRadius:10}} source={{uri:'https://punfuel.pythonanywhere.com/media/'+data[item].profile_pic}}/>
                     
-                    <Text style={{textAlign:'center',fontSize:18}}>Be the first to comment for this post</Text>
-                </View>    
-            );
-        }}
-        ListFooterComponent={()=>{
-            return(
-            <FooterComponent item={data}/>
-            );
-        }}
-        />
-        )}
+                    <Text style={{marginRight:3,color:'#23272A',fontWeight:'900',fontFamily:'Roboto'}}>{(data[item].user).length>10 ?(data[item].user).slice(0,10)+'...':((data[item].user))}</Text>
+                    {data[item].verified==true &&
+                    <Image style={{width:16,height:16,marginTop:3,marginLeft:3}} source={{uri:'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/twitter_verified.png'}}/>
+                    }
+                </View>
+                <View style={{flexDirection:'column',marginLeft:55,top:-17}}>
+                        <Text style={{fontSize:16}}>{data[item].comment}</Text>
+                </View>
+            </View>
+             );
+         })    
+         }
+        {data.length>7 && <FooterComponent/>} 
+        </ScrollView> 
+        
         <FixedBottom>
             <TouchableOpacity  onPress={()=>{onOpen()}}>
             <View style={{height:30,width:'100%',marginBottom:24}}>
@@ -123,6 +145,7 @@ export default function StackComment({route}){
             </View>
             </TouchableOpacity>
         </FixedBottom>
+       
         </View>
         <Modalize ref={modalizeRef} snapPoint={400} modalHeight={500}>
             <View style={{flexDirection:'column'}}>

54fc47ff66a171d1670bb0ea1d2f45a25ea1da65
Sreedhar <sreedharks2003@gmail.com>
2021-11-10 00:17:12 +0530

    new component tree

--- Components/screens/StackProfile.js
+++ Components/screens/StackProfile.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import {View} from 'react-native';
+import React from 'react';
+import {View,ScrollView} from 'react-native';
 import { Appbar } from 'react-native-paper';
 import ProfileCardComponent from '../ProfileCardComponent';
 import PostComponent from '../PostComponent';
@@ -18,26 +18,26 @@ export default function StackProfile({navigation,route}){
         
     
        
- 
-          
-        <PostComponent apiUrl={'https://punfuel.pythonanywhere.com/user-posts/'+route.params.username+'/?limit='} 
-          topheader={()=>{
-           return(
-            <View style={{flex:1}}>   
+        <View>  
+   
+        <PostComponent apiUrl={'https://punfuel.pythonanywhere.com/user-posts/'+route.params.username+'/?limit=10'} 
+         
+           
+           item={route.params.t}
+           user={route.params.username}
+           topheader={()=>{
+               return(
+                <View style={{flex:1}}>   
                 <ProfileCardComponent 
                 item={token} 
                 url={'https://punfuel.pythonanywhere.com/accounts/api/profile/'+route.params.username+'/'}
                 
                 />
-           </View>
-            );
+               </View> 
+               );
            }}
-           issticky={0.1} 
-           item={route.params.t}
-           user={route.params.username}
-           
         />
-    
+       </View>
     </View>      
     );
 }
